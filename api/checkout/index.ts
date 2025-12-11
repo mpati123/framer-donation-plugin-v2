b47f0d3f331754cac5d2ef4e320666d4fd592501
Patrycja Nehrebecka <patrycja.nehrebecka@nekken.pl>
2025-12-11 20:16:05 +0100

    Use SERVICE_ROLE_KEY for checkout insert

--- api/checkout/index.ts
+++ api/checkout/index.ts
@@ -14,7 +14,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 const supabase = createClient(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY!
 )
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
